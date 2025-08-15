fe461949b6b6dd4766ccd4067acb0ebdf8636e5f
yikenman <17548672+yikenman@users.noreply.github.com>
2025-08-15 15:55:46 +0800

    docs(changeset): test: fix case

--- packages/python-pptx-wasm/src/bridge/bridge.spec.ts
+++ packages/python-pptx-wasm/src/bridge/bridge.spec.ts
@@ -60,7 +60,10 @@ describe('init_pyodide_bridge', () => {
 
     await init_pyodide_bridge(pyodideMock);
 
-    expect(pyodideMock.loadPackage).toHaveBeenCalledWith('python-pptx');
+    expect(pyodideMock.loadPackage).toHaveBeenCalledWith('python-pptx', {
+      errorCallback: expect.any(Function),
+      messageCallback: expect.any(Function)
+    });
     expect(pyodideMock.runPython).toHaveBeenCalled();
 
     expect(core_funs.create_presentation).toBe('create_presentation');
